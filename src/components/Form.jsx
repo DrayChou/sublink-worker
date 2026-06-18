c0c8cd5ba2a86a4ed61c106c89b7bfd36b1f6771
dray <dray@233.wf>
2026-06-18 15:20:16 +0800

    fix: make node filter toggle clickable

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -226,10 +226,10 @@ export const Form = (props) => {
                 </h3>
                 <p class="text-sm text-gray-500 dark:text-gray-400 mt-1">{t('nodeFilterTip')}</p>
               </div>
-              <div class="relative inline-flex items-center cursor-pointer shrink-0">
+              <label class="relative inline-flex items-center cursor-pointer shrink-0">
                 <input type="checkbox" x-model="nodeFilterEnabled" class="sr-only peer" />
                 <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-300 dark:peer-focus:ring-primary-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary-600"></div>
-              </div>
+              </label>
             </div>
 
             <div
